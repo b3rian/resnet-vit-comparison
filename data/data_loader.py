6a94e6eb7a9069351796e999dbbc6ed5ba3cd756
b3rian <brayann.8189@gmail.com>
2025-07-02 11:44:59 +0300

    commits

--- data/data_loader.py
+++ data/data_loader.py
@@ -1,16 +1,12 @@
-def _bytes_feature(value):
-    """Returns a bytes_list from a string / byte."""
-    if isinstance(value, str):
-        value = value.encode('utf-8')
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+import tensorflow as tf
 
-def _float_feature(value):
-    """Returns a float_list from a float / double."""
-    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+def create_example(image, label):
+    feature = {
+        'image': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image])),
+        'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[label]))
+    }
+    return tf.train.Example(features=tf.train.Features(feature=feature))
 
-def _int64_feature(value):
-    """Returns an int64_list from a bool / enum / int / uint."""
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 example = tf.train.Example(
     features=tf.train.Features(
