9f6fa12ffae756f4df93652026ea60466d6ba326
b3rian <brayann.8189@gmail.com>
2025-07-02 14:23:28 +0300

    initial commits

--- data/data_loader.py
+++ data/data_loader.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 # Data loader configuration for TensorFlow models
 AUTOTUNE = tf.data.AUTOTUNE
-IMAGE_SIZE = (224, 224)
+IMAGE_SIZE = 224
 BATCH_SIZE = 128
 NUM_CLASSES = 1000
 
@@ -20,4 +20,16 @@ def decode_example(serialized_example):
     label = tf.cast(example['image/class/label'], tf.int32) - 1  # Labels are 1-based
     return image, label
  
+# image augmentation function
+def augment_image(image, label):
+    image = tf.image.resize(image, [IMAGE_SIZE + 32, IMAGE_SIZE + 32])
+    image = tf.image.random_crop(image, [IMAGE_SIZE, IMAGE_SIZE, 3])
+    image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_brightness(image, max_delta=0.2)
+    image = tf.image.random_contrast(image, 0.8, 1.2)
+    return image, label
+
+def preprocess_image(image, label):
+    image = tf.image.resize(image, [IMAGE_SIZE, IMAGE_SIZE])
+    return image, label
 
