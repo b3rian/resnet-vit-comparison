962ee920f2c567d92dcd9cd5825ec3695edc2270
b3rian <brayann.8189@gmail.com>
2025-07-02 11:32:35 +0300

    commits

--- data/data_loader.py
+++ data/data_loader.py
@@ -11,3 +11,15 @@ def _float_feature(value):
 def _int64_feature(value):
     """Returns an int64_list from a bool / enum / int / uint."""
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+example = tf.train.Example(
+    features=tf.train.Features(
+        feature={
+            'height': _int64_feature(128),
+            'width': _int64_feature(128),
+            'depth': _int64_feature(3),
+            'label': _int64_feature(7),
+            'image_raw': _bytes_feature(image_bytes),
+        }
+    )
+)
