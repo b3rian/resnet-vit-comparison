c56761a08aaf8eef15f5c2e267d8cf5dd9f758bf
b3rian <brayann.8189@gmail.com>
2025-07-03 18:17:23 +0300

    edited

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ try:
     strategy = tf.distribute.TPUStrategy(tpu)
 except ValueError:
     strategy = tf.distribute.get_strategy()
+
+train_ds = get_dataset("/content/train.tfrecord", training=True, batch_size=128)
+val_ds = get_dataset("/content/val.tfrecord", training=False, batch_size=128, repeat=False)
+
+# Define model inside strategy scope
+with strategy.scope():
+    model = tf.keras.applications.ResNet50(  # or tf.keras.models.load_model() for ViT
+        weights=None,
+        input_shape=(224, 224, 3),
+        classes=200
+    )
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train
+model.fit(train_ds, epochs=30, validation_data=val_ds, steps_per_epoch=1000, validation_steps=100)
