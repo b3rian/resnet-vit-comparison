ece30d1db8e3e62f180e827bbc6c59a5adad6257
b3rian <brayann.8189@gmail.com>
2025-07-03 08:37:36 +0300

    initial commit

--- data/data_loader.py
+++ data/data_loader.py
@@ -51,8 +51,8 @@ def build_dataset(filenames, training=True):
     dataset = dataset.prefetch(AUTOTUNE)
     return dataset
 
-TRAIN_FILENAMES = tf.io.gfile.glob("gs://imagenet-cils/TFRs/train/*.tfrecord")
-VAL_FILENAMES = tf.io.gfile.glob("gs://imagenet-cils/TFRs/validation/*.tfrecord")
+TRAIN_FILENAMES = tf.io.gfile.glob("gs://imagenet2012-tfrecord/train/*.tfrecord")
+VAL_FILENAMES = tf.io.gfile.glob("gs://imagenet2012-tfrecord/validation/*.tfrecord")
 
 train_ds = build_dataset(TRAIN_FILENAMES, training=True)
 val_ds = build_dataset(VAL_FILENAMES, training=False)
