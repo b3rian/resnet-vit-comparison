b46545288a023b8c8b411d3aeb51c0cbe6d1dc3a
b3rian <brayann.8189@gmail.com>
2025-07-03 18:12:14 +0300

    edited

--- data/input_pipeline.py
+++ data/input_pipeline.py
@@ -21,3 +21,21 @@ def augment(image, label):
     image = tf.image.random_contrast(image, 0.8, 1.2)
     image = tf.image.random_crop(tf.image.resize_with_crop_or_pad(image, 256, 256), [IMAGE_SIZE, IMAGE_SIZE, 3])
     return image, label
+
+def get_dataset(tfrecord_path, batch_size=128, training=True, cache=True, shuffle_buffer=2048, repeat=True):
+    dataset = tf.data.TFRecordDataset(tfrecord_path)
+    dataset = dataset.map(parse_example, num_parallel_calls=tf.data.AUTOTUNE)
+
+    if cache:
+        dataset = dataset.cache()
+
+    if training:
+        dataset = dataset.map(augment, num_parallel_calls=tf.data.AUTOTUNE)
+        dataset = dataset.shuffle(shuffle_buffer)
+
+    if repeat:
+        dataset = dataset.repeat()
+
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+    return dataset
