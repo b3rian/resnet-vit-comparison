41161d144528934d9d44d0d073d9cc0127b9de10
b3rian <brayann.8189@gmail.com>
2025-07-03 18:11:34 +0300

    edited

--- data/input_pipeline.py
+++ data/input_pipeline.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-IMAGE_SIZE = 224
+IMAGE_SIZE = 64
 NUM_CLASSES = 200
 
 def parse_example(example_proto):
@@ -13,4 +13,11 @@ def parse_example(example_proto):
     image = tf.image.resize(image, [IMAGE_SIZE, IMAGE_SIZE])
     image = tf.cast(image, tf.float32) / 255.0
     label = tf.one_hot(parsed['label'], NUM_CLASSES)
+    return image, label
+
+def augment(image, label):
+    image = tf.image.random_flip_left_right(image)
+    image = tf.image.random_brightness(image, 0.2)
+    image = tf.image.random_contrast(image, 0.8, 1.2)
+    image = tf.image.random_crop(tf.image.resize_with_crop_or_pad(image, 256, 256), [IMAGE_SIZE, IMAGE_SIZE, 3])
     return image, label
