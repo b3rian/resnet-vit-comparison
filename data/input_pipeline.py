78fee7bafd646524546af7b82caccd4e44b63a63
b3rian <brayann.8189@gmail.com>
2025-07-04 11:42:02 +0300

    edited

--- data/input_pipeline.py
+++ data/input_pipeline.py
@@ -10,12 +10,14 @@ def decode_img(img):
     img = tf.image.resize(img, IMAGE_SIZE)
     return img
 
-def augment(image, label):
-    image = tf.image.random_flip_left_right(image)
-    image = tf.image.random_brightness(image, 0.2)
-    image = tf.image.random_contrast(image, 0.8, 1.2)
-    image = tf.image.random_crop(tf.image.resize_with_crop_or_pad(image, 256, 256), [IMAGE_SIZE, IMAGE_SIZE, 3])
-    return image, label
+def process_train_image(file_path, label):
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    img = tf.image.random_flip_left_right(img)
+    img = tf.image.random_brightness(img, max_delta=0.1)
+    img = tf.image.random_contrast(img, lower=0.9, upper=1.1)
+    img = img / 255.0  # Normalize to [0,1]
+    return img, tf.one_hot(label, NUM_CLASSES)
 
 def get_dataset(tfrecord_path, batch_size=128, training=True, cache=True, shuffle_buffer=2048, repeat=True):
     dataset = tf.data.TFRecordDataset(tfrecord_path)
