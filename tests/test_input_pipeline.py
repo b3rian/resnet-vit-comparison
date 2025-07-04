c27c4dd79c6a4fc9f7d6c458370196aa80bf8aa7
b3rian <brayann.8189@gmail.com>
2025-07-04 12:46:40 +0300

    edited

--- tests/test_input_pipeline.py
+++ tests/test_input_pipeline.py
@@ -3,6 +3,13 @@ import tensorflow as tf
 import input_pipeline
 import os
 
+DATA_DIR = "/content/drive/MyDrive/tiny-imagenet-200"
+
+@pytest.fixture(scope="module")
+def datasets():
+    batch_size = 32
+    train_ds, val_ds = input_pipeline.get_datasets(DATA_DIR, batch_size=batch_size)
+    return train_ds, val_ds
 
      
 
