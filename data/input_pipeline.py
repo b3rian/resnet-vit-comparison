74c3e6e0cfdd9bbb7038dbb51ff26f7c59748cde
b3rian <brayann.8189@gmail.com>
2025-07-04 11:52:56 +0300

    edited

--- data/input_pipeline.py
+++ data/input_pipeline.py
@@ -19,20 +19,8 @@ def process_train_image(file_path, label):
     img = img / 255.0  # Normalize to [0,1]
     return img, tf.one_hot(label, NUM_CLASSES)
 
-def get_dataset(tfrecord_path, batch_size=128, training=True, cache=True, shuffle_buffer=2048, repeat=True):
-    dataset = tf.data.TFRecordDataset(tfrecord_path)
-    dataset = dataset.map(parse_example, num_parallel_calls=tf.data.AUTOTUNE)
-
-    if cache:
-        dataset = dataset.cache()
-
-    if training:
-        dataset = dataset.map(augment, num_parallel_calls=tf.data.AUTOTUNE)
-        dataset = dataset.shuffle(shuffle_buffer)
-
-    if repeat:
-        dataset = dataset.repeat()
-
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
-    return dataset
+def process_val_image(file_path, label):
+    img = tf.io.read_file(file_path)
+    img = decode_img(img)
+    img = img / 255.0  # Normalize to [0,1]
+    return img, tf.one_hot(label, NUM_CLASSES)
