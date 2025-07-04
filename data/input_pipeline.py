7a1234f58353a6a2e7cb730aca65ba9ef7ed5597
b3rian <brayann.8189@gmail.com>
2025-07-04 11:34:23 +0300

    edited

--- data/input_pipeline.py
+++ data/input_pipeline.py
@@ -5,17 +5,10 @@ AUTOTUNE = tf.data.AUTOTUNE
 IMAGE_SIZE = (64, 64)
 NUM_CLASSES = 200
 
-def parse_example(example_proto):
-    features = {
-        'image': tf.io.FixedLenFeature([], tf.string),
-        'label': tf.io.FixedLenFeature([], tf.int64)
-    }
-    parsed = tf.io.parse_single_example(example_proto, features)
-    image = tf.io.decode_jpeg(parsed['image'], channels=3)
-    image = tf.image.resize(image, [IMAGE_SIZE, IMAGE_SIZE])
-    image = tf.cast(image, tf.float32) / 255.0
-    label = tf.one_hot(parsed['label'], NUM_CLASSES)
-    return image, label
+def decode_img(img):
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, IMAGE_SIZE)
+    return img
 
 def augment(image, label):
     image = tf.image.random_flip_left_right(image)
