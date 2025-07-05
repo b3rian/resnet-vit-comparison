534072e60e996c796acf7bc35a6fc682b59dc54d
b3rian <brayann.8189@gmail.com>
2025-07-05 14:47:02 +0300

    edited

--- models/vgg-19.py
+++ models/vgg-19.py
@@ -1,5 +1,15 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
-def VGG19(input_shape=(224, 224, 3), num_classes=1000):
+def VGG19(input_shape=(64, 64, 3), num_classes=200):
     model = models.Sequential()
+
+    # Block 1
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=input_shape))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2), strides=2))
+
+    # Block 2
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2), strides=2))
