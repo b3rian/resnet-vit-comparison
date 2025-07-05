b90d2d66b136ab4325de96db16618b3553613d21
b3rian <brayann.8189@gmail.com>
2025-07-05 16:39:14 +0300

    edited

--- models/resnet.py
+++ models/resnet.py
@@ -16,7 +16,7 @@ class BasicBlock(tf.keras.Model):
         self.bn2 = layers.BatchNormalization()
         self.downsample = downsample
 
-     def call(self, x, training=False):
+    def call(self, x, training=False):
         identity = x
 
         out = self.conv1(x)
@@ -33,3 +33,23 @@ class BasicBlock(tf.keras.Model):
         out = self.relu(out)
 
         return out
+    
+# ResNet class
+class ResNet(Model):
+    def __init__(self, block, layers_units, num_classes=1000):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+
+        self.conv1 = layers.Conv2D(64, kernel_size=7, strides=2,
+                                   padding='same', use_bias=False)
+        self.bn1 = layers.BatchNormalization()
+        self.relu = layers.ReLU()
+        self.maxpool = layers.MaxPooling2D(pool_size=3, strides=2, padding='same')
+
+        self.layer1 = self._make_layer(block, 64, layers_units[0])
+        self.layer2 = self._make_layer(block, 128, layers_units[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers_units[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers_units[3], stride=2)
+
+        self.avgpool = layers.GlobalAveragePooling2D()
+        self.fc = layers.Dense(num_classes)
